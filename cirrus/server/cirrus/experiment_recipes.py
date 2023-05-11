dc66701cbf75951d7386cd90ec2511ab70e4d0a3
yashikakhurana <yashikakhuranayashika@gmail.com>
2023-05-11 14:41:13 -0700

    fix #8802 feat(cirrus): integrate sdk

--- cirrus/server/cirrus/experiment_recipes.py
+++ cirrus/server/cirrus/experiment_recipes.py
@@ -1,13 +1,47 @@
-from typing import Any, Dict, List
+import logging
+from typing import Any, Dict, List, cast
+
+import requests
+from decouple import config  # type: ignore
+from cirrus_sdk import CirrusClient
+
+import json
+
+logger = logging.getLogger(__name__)
+
+
+context = json.dumps(
+    {
+        "app_id": "fenix",
+        "app_name": "fenix",
+        "channel": "developer",
+    }
+)
+client = CirrusClient(context)
 
 
 class RemoteSettings:
-    recipes: List[Dict[str, Any]] = []
+    recipes: List[Dict[str, Any]] = {"data": []}
+    url: str = cast(str, config("REMOTE_SETTING_URL", default=""))
 
     @classmethod
     def get_recipes(cls) -> List[Dict[str, Any]]:
         return cls.recipes
 
-    @classmethod
-    def update_recipes(cls, new_recipes: List[Dict[str, Any]]) -> None:
-        cls.recipes = new_recipes
+    def update_recipes(self, new_recipes: List[Dict[str, Any]]) -> None:
+        self.recipes = new_recipes
+        client.set_experiments(json.dumps(self.recipes))
+
+    def fetch_recipes(self):
+        try:
+            response = requests.get(self.url)
+            if response.status_code == 200:
+                data = response.json().get("data", [])
+                if data:
+                    self.update_recipes({"data": data})
+                    logger.info("Fetched resources")
+                else:
+                    logger.warning("No recipes found in the response")
+        except requests.exceptions.RequestException as e:
+            logger.error(f"Failed to fetch recipes: {e}")
+            raise e
--- cirrus/server/cirrus/main.py
+++ cirrus/server/cirrus/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI, status
-
-from .experiment_recipes import RemoteSettings
+import json
+from datetime import datetime
+from .experiment_recipes import RemoteSettings, client
 from .feature_manifest import FeatureManifestLanguage as FML
 from .manifest_loader import ManifestLoader
 from .sdk import SDK
+
+
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 remote_setting = RemoteSettings()
@@ -12,20 +16,81 @@ manifest_loader = ManifestLoader()
 fml = FML()
 
 
+remote_setting_refresh_rate_in_seconds: int = int(
+    config("REMOTE_SETTING_REFRESH_RATE_IN_SECONDS", default=10)  # type: ignore
+)
+
+
+# Set up scheduler with ThreadPoolExecutor and configure job options
+scheduler: AsyncIOScheduler = AsyncIOScheduler(
+    job_defaults={"coalesce": False, "max_instances": 3, "misfire_grace_time": 60},
+
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "World", "client": client, "r": remote_setting.get_recipes()}
 
 
-@app.get("/enrollment_status", status_code=status.HTTP_200_OK)
-async def compute_enrollment_status():
-    recipes = remote_setting.get_recipes()
-    # will recieve as part of incoming request
-    targeting_context = {"client_id": "testid"}
-    enrolled_partial_configuration = sdk.compute_enrollments(recipes, targeting_context)
-    feature_configurations = manifest_loader.get_latest_feature_manifest()
-    client_feature_configuration = fml.compute_feature_configurations(
-        enrolled_partial_configuration, feature_configurations
+
+@app.get("/v1/features/", status_code=status.HTTP_200_OK)
+async def compute_features():
+    t = datetime.now()
+
+    res = client.handle_enrollment(
+        json.dumps({"clientId": "test", "requestContext": {}})
     )
+    ts = datetime.now()
+    print(ts - t)
+    return json.loads(res)
 
-    return client_feature_configuration
+
+# filter out experiments
+# force to run on start up
+# set experiments should be updated whenever new copy of recipes received
+# move .env value
+
+# will recieve as part of incoming request
+
+# targeting_context = {"client_id": "testid"}
+# enrolled_partial_configuration = sdk.compute_enrollments(recipes, targeting_context)
+# feature_configurations = manifest_loader.get_latest_feature_manifest()
+# client_feature_configuration = fml.compute_feature_configurations(
+#     enrolled_partial_configuration, feature_configurations
+# )
+# return client_feature_configuration
+
+
+@app.on_event("startup")
+async def start_scheduler():
+    scheduler.start()
+    app.state.scheduler = scheduler
+
+
+@app.on_event("shutdown")
+async def shutdown_scheduler():
+    scheduler.shutdown()
+
+
+async def fetch_schedule_recipes() -> None:
+    try:
+        remote_setting.fetch_recipes()
+    except Exception as e:
+        # If an exception is raised, log the error and schedule a retry
+        logger.error(f"Failed to fetch recipes: {e}")
+        scheduler.add_job(  # type: ignore
+            fetch_schedule_recipes,
+            "interval",
+            seconds=remote_setting_refresh_rate_in_seconds,
+            max_instances=1,
+            max_retries=3,
+        )
+
+
+# Schedule the initial job to fetch recipes
+scheduler.add_job(  # type: ignore
+    fetch_schedule_recipes,
+    "interval",
+    seconds=remote_setting_refresh_rate_in_seconds,
+    max_instances=1,
+)
+
